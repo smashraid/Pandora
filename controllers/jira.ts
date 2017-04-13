11b2a1cf22b6eee325fbaf37f15914ad767d49dd
Saulo Tsuchida <Saulo.Tsuchida@cignium.com>
2017-04-12 20:35:38 -0500

    add jira service

--- controllers/jira.ts
+++ controllers/jira.ts
@@ -1,8 +1,22 @@
 import { Request, Response } from "express";
+import * as requestService from "request";
  
 export class JiraController {
     get(req: Request, res: Response): void {
         let name: string = req.params.name;
+        let user: string = "STsuchida";
+        requestService({
+            uri: "https://jira.cignium.com/rest/api/2/search",
+            method: "POST",
+            headers: {            
+                "Content-Type": "application/json",
+                "Authorization":"Basic c3RzdWNoaWRhOlJ5dVQwJDQxcjAyNQ==",
+                "body": JSON.stringify({
+                    jql: `assignee = ${user} AND status != Accepted  AND resolution = Unresolved  order by updated DESC`,
+                    fields: ["summary"]
+                })
+            }
+        })
         res.json({
             message: `Hello ${name}`
         });
